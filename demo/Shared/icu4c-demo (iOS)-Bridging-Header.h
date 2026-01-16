3a730032e2bf5e450df4552b4a29a1fe7d17f366
apotocki <sane22222@aim.com>
2026-01-16 18:59:37 +0100

    +demo project

---
+++ demo/Shared/icu4c-demo (iOS)-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include <stdlib.h>
+
+int toUpper(const char* str, char* rstr, uint32_t sz);
---
+++ demo/icu4c-demoTests/icu4c_demoTests.swift
@@ -0,0 +1,31 @@
+//
+//  icu4c_demoTests.swift
+//  icu4c-demoTests
+//
+//  Created by Alexander Pototskiy on 20.04.21.
+//
+
+import XCTest
+@testable import icu4c_demo
+
+class icu4c_demoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(icuToUpper("test_ПрЮвет!"), "TEST_ПРЮВЕТ!", "wrong")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
